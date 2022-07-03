0acc0169f7825b8b1732487eccacc26df8130343
Mohamed Abdelmenam <mabdelmenam@gmail.com>
2022-07-03 03:14:21 -0400

    Added requirements.txt and gas_tracker.py

---
+++ gas_tracker.py
@@ -0,0 +1,7 @@
+#use API to get the gas price
+#create nromal gui to display
+#design, make transparent
+#figure out how to make it stuck on a different corner of the screen
+#make options for where somneone would want to display, bottom right, top right, top left, bottom left
+#make an opacity slider or change option
+#make executable
